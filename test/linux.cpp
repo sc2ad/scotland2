d5c62e429eaa4b1586d60ccc8addd83bea1a6c4a
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-09-04 14:54:52 -0400

    WSL executable and tests

---
+++ test/linux.cpp
@@ -0,0 +1,36 @@
+#ifdef LINUX_TEST
+
+#include <iostream>
+#include "loader.hpp"
+
+void logDependencies(std::span<modloader::Dependency const> dependencies, size_t indent = 1) {
+    for (auto const& dep : dependencies) {
+        std::cout << std::string(indent * 3, ' ') << '-' << dep.object.path.filename() << std::endl;
+        logDependencies(dep.dependencies, indent + 1);
+    }
+}
+
+int main() {
+
+    auto path = std::filesystem::current_path();
+    auto mod = modloader::SharedObject(path/"test"/"libchroma.so");
+
+
+    std::cout << "Loading " << mod.path.c_str() << std::endl;
+
+    auto dependencies = mod.getToLoad(modloader::LoadPhase::Mods);
+
+    for (auto const& dep : dependencies) {
+        std::cout << "-" << dep.object.path.filename() << std::endl;
+        logDependencies(dep.dependencies);
+    }
+
+    std::cout << std::string(3, '\n') << "Sorted:" << std::endl;
+
+    auto sorted = modloader::topologicalSort(dependencies);
+    for (auto const& dep : dependencies) {
+        std::cout << "-" << dep.object.path.filename();
+    }
+}
+
+#endif
