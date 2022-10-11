00ed336a6c6fc1f251bba216444191c4c6dcf1eb
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2022-10-11 16:53:28 -0400

    Disable load tests on Linux

--- test/linux.cpp
+++ test/linux.cpp
@@ -3,17 +3,15 @@
 #include "internal-loader.hpp"
 #include "tests.hpp"
 
-
 int main() {
-    auto dependencyPath = std::filesystem::current_path()/"test";
-    auto modPath = dependencyPath/"mods"/"libchroma.so";
+    auto dependencyPath = std::filesystem::current_path() / "test";
+    auto modPath = dependencyPath / "mods" / "libchroma.so";
     // Load
 
     auto deps = tests::getDependencyTreeTest(dependencyPath, modPath);
     tests::sortDependencyTreeTest(deps);
 
-    tests::loadModsTest(dependencyPath);
-
+    // tests::loadModsTest(dependencyPath);
 }
 
 #endif
