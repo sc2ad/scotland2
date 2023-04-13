23dbfc0a6e348fa95d2e9f4a2e2705179719637d
Sc2ad <adamznow@gmail.com>
2023-04-12 22:08:24 -0700

    Small cleanups to tody and scripts
    Script now properly allows for windows support

--- build.ps1
+++ build.ps1
@@ -1,10 +1,10 @@
 
 function Clean-Build-Folder {
-    if (Test-Path -Path "build")
-    {
+    if (Test-Path -Path "build") {
         remove-item build -R
         new-item -Path build -ItemType Directory
-    } else {
+    }
+    else {
         new-item -Path build -ItemType Directory
     }
 }
@@ -21,7 +21,7 @@ $ExitCode = $LastExitCode
 
 # Post build, we actually want to transform the compile_commands.json file such that it has no \\ characters and ONLY has / characters
 (Get-Content -Path build/compile_commands.json) |
-    ForEach-Object {$_ -Replace '\\\\', '/'} | Set-Content -Path build/compile_commands.json
+ForEach-Object { $_ -Replace '\\\\', '/' } | ForEach-Object { $_ -Replace '/\\"', '\\\"' } | Set-Content -Path build/compile_commands.json
 
 
 exit $ExitCode
--- shared/_config.h
+++ shared/_config.h
@@ -1,4 +1,8 @@
 #pragma once
 
-#define MODLOADER_FUNC extern "C" __attribute__((visibility("default")))
 #define MODLOADER_EXPORT __attribute__((visibility("default")))
+#ifdef __cplusplus
+#define MODLOADER_FUNC extern "C" __attribute__((visibility("default")))
+#else
+#define MODLOADER_FUNC MODLOADER_EXPORT
+#endif
