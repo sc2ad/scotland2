538f1101fb740ee393fcf5eccfcdade35f4c8f75
Sc2ad <adamznow@gmail.com>
2023-10-14 16:44:01 -0700

    Improve modloader API documentation

--- shared/modloader.h
+++ shared/modloader.h
@@ -11,6 +11,9 @@
 
 namespace modloader {
 
+// For all functions here, see their corresponding C API functions for documentation
+// These serve as a C++ friendly interface to avoid conversions and copies
+
 MODLOADER_EXPORT std::filesystem::path const& get_modloader_path() noexcept;
 MODLOADER_EXPORT std::filesystem::path const& get_modloader_root_load_path() noexcept;
 MODLOADER_EXPORT std::filesystem::path const& get_files_dir() noexcept;
@@ -37,16 +40,31 @@ typedef enum {
 #ifdef __cplusplus
 }
 #endif
-
+/// @brief Returns true if the modloader failed to copy over the libs/mods to load, false otherwise
 MODLOADER_FUNC bool modloader_get_failed();
+/// @brief The captured pointer to the JavaVM
 MODLOADER_EXPORT extern JavaVM* modloader_jvm;
+/// @brief The captured dlopen-d libil2cpp.so handle
 MODLOADER_EXPORT extern void* modloader_libil2cpp_handle;
+/// @brief The captured dlopen-d libunity.so handle
 MODLOADER_EXPORT extern void* modloader_unity_handle;
+/// @brief Returns the path of the modloader.
+/// Example output: /data/user/0/com.beatgames.beatsaber/files/libsl2.so
 MODLOADER_FUNC char const* modloader_get_path();
+/// @brief Returns the root load path for searches.
+/// Example output: /sdcard/ModData/com.beatgames.beatsaber/Modloader
 MODLOADER_FUNC char const* modloader_get_root_load_path();
+/// @brief Returns the path to the files directory
+/// Example output: /data/user/0/com.beatgames.beatsaber/files
 MODLOADER_FUNC char const* modloader_get_files_dir();
+/// @brief Returns the path to the external folder
+/// Example output: /storage/emulated/0/Android/data/com.beatgames.beatsaber/files
 MODLOADER_FUNC char const* modloader_get_external_dir();
+/// @brief Returns the application ID
+/// Example output: com.beatgames.beatsaber
 MODLOADER_FUNC char const* modloader_get_application_id();
+/// @brief Returns the path where the modloader was found from
+/// Example output: /sdcard/ModData/com.beatgames.beatsaber/Modloader/libsl2.so
 MODLOADER_FUNC char const* modloader_get_source_path();
 /// @brief Triggers an unload of the specified mod, which will in turn call the unload() method of it.
 /// It will also be removed from any collections. It is UB if the mod to be unloaded is the currently executing mod.
